1077bb7cec9bb92544f4d07c515063d52fbee395
asy <lucky_asyxu@163.com>
2018-01-21 18:47:03 +0800

    MOD:调整导航栏样式

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-
-
   $("#companys-active").mixItUp();
-
 
   $("#carousel-slider").owlCarousel({
     paginationSpeed: 500,
@@ -10,15 +7,27 @@ $(document).ready(function () {
     autoPlay: 3000,
   });
 
+  let lis = $('.navbar-nav li'); // 得到的 lis 是 object
+  let pathname = window.location.pathname;
+  for (let key in Object.keys(lis)) {
+    let item = lis[key];
+    if (item && item.getAttribute) {
+      let nav = item.getAttribute('data-nav');
 
+      if (nav === pathname || (pathname.startsWith(nav+'/'))) {
+        $(item).addClass('active');
+      } else {
+        $(item).removeClass('active');
+      }
+    }
+  }
 
-
-  $("#clients-logo").owlCarousel({
-    autoPlay: 3000,
-    items: 5,
-    itemsDesktop: [1199, 5],
-    itemsDesktopSmall: [979, 5],
-  });
+  // $("#clients-logo").owlCarousel({
+  //   autoPlay: 3000,
+  //   items: 5,
+  //   itemsDesktop: [1199, 5],
+  //   itemsDesktopSmall: [979, 5],
+  // });
 
   // $("#works-logo").owlCarousel({
   //   autoPlay: 3000,
@@ -27,24 +36,20 @@ $(document).ready(function () {
   //   itemsDesktopSmall: [979, 5],
   // });
 
-
   // google map
-  var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
-    });
-  }
-
+  // var map;
+  // function initMap() {
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: { lat: -34.397, lng: 150.644 },
+  //     zoom: 8
+  //   });
+  // }
 
   // Counter
-
   $('.counter').counterUp({
     delay: 10,
     time: 1000
   });
-
 
 });
 
